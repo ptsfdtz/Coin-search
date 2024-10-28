f789c75994131a210ae0980d9e794472bd9782b7
ptsfdtz <pitousanfadetuzi@gmail.com>
2024-10-28 11:23:54 +0800

    feat:add API request

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-coin-search
 
+copy from youtube video:
+
+[Jump to](https://www.youtube.com/watch?v=gxXw-M5lDOw&list=WL&index=1&t=227s)
+
--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Coins from './components/Coins';
 
 function App() {
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+      console.log(response.data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }, [])
+
   return (
     <>
-
+      <Coins coins={coins} />
     </>
   );
 }
