e7aedc98601889036c8a935e8f82bec44c8e97c0
ptsfdtz <pitousanfadetuzi@gmail.com>
2024-10-29 22:18:54 +0800

    feat:add routes

--- src/components/CoinItem/CoinItem.jsx
+++ src/components/CoinItem/CoinItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../Coins/Coins.css'
+import './CoinItem.css'
+
 const CoinItem = (props) => {
     return (
         <div className='coin-row'>
@@ -8,7 +10,7 @@ const CoinItem = (props) => {
                 <img src={props.coins.image} alt="coin-img" />
                 <p>{props.coins.symbol.toUpperCase()}</p>
             </div>
-            <p>${props.coins.current_price.toLocaleString()}</p>
+            <p className='coin-price'>${props.coins.current_price.toLocaleString()}</p>
             <p>{props.coins.price_change_percentage_24h.toFixed(2)}%</p>
             <p className='hide-mobile'>${props.coins.total_volume.toLocaleString()}</p>
             <p className='hide-mobile'>${props.coins.market_cap.toLocaleString()}</p>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { FaCoins } from 'react-icons/fa'
 import './Navbar.css'
-import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div>
             <div className='navbar'>
                 <FaCoins className='icon' />
-                <h1>Coin <span className='purple'>Search</span></h1>
+                <a href="/"><h1>Coin <span className='purple'>Search</span></h1></a>
             </div>
         </div>
     )
